294cb0dafbc720b792b9f72e4bc34578b17ace8c
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2022-10-22 22:18:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import path from 'path';
 const __dirname = path.resolve();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4444;
 app.use(express.static(path.join(__dirname, '/files')));
 app.use(cors());
 
